cece8c6c311e861e20b006370eb3cea830b36f96
MauroGBecares <maurobecares26@gmail.com>
2025-02-23 23:55:26 -0300

    Agregar archivos de proyecto.

---
+++ DesignPatternsNet/Program.cs
@@ -0,0 +1,13 @@
+﻿using Singleton;
+
+// PATRONES DE DISEÑO
+
+// SINGLETON
+// Es un patrón de diseño creacional que garantiza que una clase tenga una única instancia en toda la aplicación
+// y proporciona un punto de acceso global a ella.
+// Esto garantiza mejor redimiento ya que evita la creación de multiples instancias de la misma clase cuando no es necesario.
+
+var log = Log.GetInstance();
+log.Save("Se inicializo el programa");
+log.Save("Se guardo un archivo");
+log.Save("Se cerro el programa");
---
+++ Singleton/Log.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Log
+    {
+        private static Log _instance = null;
+
+        private readonly static object _lock = new object();
+        private string _path = "log.txt";
+
+        public static Log GetInstance()
+        {
+            // El lock se utiliza para evitar que se creen multiples instancias de la clase en caso de que se llame al metodo
+            // GetInstance() desde multiples hilos.
+            lock (_lock)
+            {
+                if (_instance == null)
+                {
+                    _instance = new Log();
+                }
+            }
+            return _instance;
+        }
+        private Log()
+        {
+
+        }
+
+        public void Save(string message)
+        {
+            File.AppendAllText(_path, message + Environment.NewLine);
+            Console.WriteLine($"Se guardo el log a {_path}: " + message);
+        }
+    }
+}
